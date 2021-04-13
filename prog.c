e688114b989fc887920dc3f8cc32161d0b2c82fa
Kevin Browne <kevinbrowne@Kevins-iMac.local>
2021-04-13 16:20:04 -0400

    Switched to recursive version of algorithm

--- prog.c
+++ prog.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
+int fib(int n);
+
 // Code Example from:
 //   https://www.programiz.com/c-programming/examples/fibonacci-series
 int main() {
 
-    int i, n, t1 = 0, t2 = 1, nextTerm;
+    int i, n;
     printf("Enter the number of terms: ");
     scanf("%d", &n);
     printf("Fibonacci Series: ");
 
     for (i = 1; i <= n; ++i) {
-        printf("%d, ", t1);
-        nextTerm = t1 + t2;
-        t1 = t2;
-        t2 = nextTerm;
+        printf("%d, ", fib(i));
     }
 
     return 0;
 }
+
+int fib(int n)
+{
+   if (n <= 1) return n;
+   return fib(n-1) + fib(n-2);
+}
+
